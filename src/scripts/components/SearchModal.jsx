025f8282774ed2c5100c7d8d2a97472e8b4ae3d4
Socrates <nsocrates.dev@gmail.com>
2015-12-10 20:49:18 -0800

    Create toggle method for modal, create new elements

--- src/scripts/components/SearchModal.jsx
+++ src/scripts/components/SearchModal.jsx
@@ -13,16 +13,24 @@ export default React.createClass({
 	},
 
 	toggleSearch: function() {
+		const mSearchBar = document.getElementById('m-searchbar');
 		let transformed = !this.state.isTransformed;
 		this.setState({ isTransformed: transformed });
+		mSearchBar.focus();
 		document.body.classList.toggle('body-overflow');
 	},
 
 	render: function() {
 		return (
-			<div className="control" onClick={this.toggleSearch} >
-				<div className="modal"></div>
-				<i className="fa fa-search"></i>
+			<div className="m-search">
+				<div className="m-control">
+					<div className="m-modal"></div>
+					<i className="fa fa-search" onClick={this.toggleSearch} ></i>
+					<i className="fa fa-times" onClick={this.toggleSearch} ></i>
+				</div>
+				<form className="form-group">
+					<input id="m-searchbar" className="m-searchbar" type="text" placeholder="Looking for something...?" />
+				</form>
 			</div>
 		);
 	}
