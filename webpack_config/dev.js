b8ad26953c20c120651ae2859fca12c362250644
Socrates <nsocrates.dev@gmail.com>
2016-01-04 14:35:33 -0800

    Update package.json & .gitignore

--- webpack_config/dev.js
+++ webpack_config/dev.js
@@ -6,28 +6,25 @@ var _ = require('lodash');
 
 var baseConfig = require('./base');
 
-// Add needed plugins here
-//
-
 var config = _.merge({
-  entry: [
-    'webpack-dev-server/client?http://localhost:8000',
-    'webpack/hot/only-dev-server',
-    './src/scripts/index'
-  ],
-  cache: true,
-  devtool: 'eval',
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ]
+	entry: [
+		'webpack-dev-server/client?http://localhost:8000',
+		'webpack/hot/only-dev-server',
+		'./src/scripts/index'
+	],
+	cache: true,
+	devtool: 'eval',
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoErrorsPlugin()
+	]
 }, baseConfig);
 
 // Add needed loaders
 config.module.loaders.push({
-  test: /\.(js|jsx)$/,
-  loader: 'react-hot!babel-loader',
-  include: path.join(__dirname, '/../src')
+	test: /\.(js|jsx)$/,
+	loader: 'react-hot!babel-loader',
+	include: path.join(__dirname, '/../src')
 });
 
 module.exports = config;
--- webpack_config/dist.js
+++ webpack_config/dist.js
@@ -10,25 +10,25 @@ var baseConfig = require('./base');
 //
 
 var config = _.merge({
-  entry: path.join(__dirname, '../src/scripts/index'),
-  cache: false,
-  devtool: 'sourcemap',
-  plugins: [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"production"'
-    }),
-    new webpack.optimize.UglifyJsPlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.AggressiveMergingPlugin(),
-    new webpack.NoErrorsPlugin()
-  ]
+	entry: path.join(__dirname, '../src/scripts/index'),
+	cache: false,
+	devtool: 'sourcemap',
+	plugins: [
+		new webpack.optimize.DedupePlugin(),
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': '"production"'
+		}),
+		new webpack.optimize.UglifyJsPlugin(),
+		new webpack.optimize.OccurenceOrderPlugin(),
+		new webpack.optimize.AggressiveMergingPlugin(),
+		new webpack.NoErrorsPlugin()
+	]
 }, baseConfig);
 
 config.module.loaders.push({
-  test: /\.(js|jsx)$/,
-  loader: 'babel',
-  include: path.join(__dirname, '/../src')
+	test: /\.(js|jsx)$/,
+	loader: 'babel',
+	include: path.join(__dirname, '/../src')
 });
 
 module.exports = config;
