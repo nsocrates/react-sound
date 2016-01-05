de9ec734b48d7dd9bf0c33f6f02ebb9180b21211
Socrates <nsocrates.dev@gmail.com>
2016-01-04 20:50:52 -0800

    Add router to Redux store

--- src/scripts/index.jsx
+++ src/scripts/index.jsx
@@ -6,17 +6,17 @@ import { routes } from './routes'
 
 require('../stylesheets/main.scss')
 
-import { loadPlaylist } from './actions/playlist'
-import { loadGenre } from './actions/genre'
-import { loadTrack } from './actions/track'
-import { loadUser } from './actions/user'
+// import { loadPlaylist } from './actions/playlist'
+// import { loadGenre } from './actions/genre'
+// import { loadTrack } from './actions/track'
+// import { loadUser } from './actions/user'
 
-export const store = makeStore()
-
-store.dispatch(loadPlaylist('405726')).then(() => console.log(store.getState()))
+// store.dispatch(loadPlaylist('405726')).then(() => console.log(store.getState()))
 // store.dispatch(loadGenre('house')).then(() => console.log(store.getState()))
 // store.dispatch(loadTrack('13158665')).then(() => console.log(store.getState()))
 // store.dispatch(loadUser('3207')).then(() => console.log(store.getState()))
+
+export const store = makeStore()
 
 ReactDOM.render(
 	<Provider store={ store }>
