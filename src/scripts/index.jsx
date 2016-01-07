3e7dc13157d966fe0fa65d99fa70c647d079e07f
Socrates <nsocrates.dev@gmail.com>
2016-01-07 14:40:54 -0800

    Delete commented code

--- src/scripts/index.jsx
+++ src/scripts/index.jsx
@@ -5,18 +5,6 @@ import makeStore from 'store/store'
 import { Provider } from 'react-redux'
 import { routes } from 'routes/routes'
 
-require('stylesheets/main.scss')
-
-// import { loadPlaylist } from './actions/playlist'
-// import { loadGenre } from './actions/genre'
-// import { loadTrack } from './actions/track'
-// import { loadUser } from './actions/user'
-
-// store.dispatch(loadPlaylist('405726')).then(() => console.log(store.getState()))
-// store.dispatch(loadGenre('house')).then(() => console.log(store.getState()))
-// store.dispatch(loadTrack('13158665')).then(() => console.log(store.getState()))
-// store.dispatch(loadUser('3207')).then(() => console.log(store.getState()))
-
 export const store = makeStore()
 
 ReactDOM.render(
