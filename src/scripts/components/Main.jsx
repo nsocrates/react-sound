1500914b165c9bd46530e0fd35d10c9bfb1bc215
Socrates <nsocrates.dev@gmail.com>
2016-01-11 00:54:32 -0800

    Refactor

--- src/scripts/components/Main.jsx
+++ src/scripts/components/Main.jsx
@@ -1,73 +1,6 @@
 import React from 'react'
-import Collection from './Collection'
 
 export default class Main extends React.Component {
-
-  getCurrentCollection() {
-    const { requested, tracksByGenre } = this.props
-
-    if (tracksByGenre.hasOwnProperty(requested)) {
-      const reqTracksIdList = this.integerToString(tracksByGenre[requested].ids)
-      const collection = this.getCollectionToRender(reqTracksIdList)
-
-      return this.renderCollection(collection)
-    }
-  }
-
-  getCollectionToRender(tracksIdList) {
-    const { trackEntity, userEntity } = this.props
-    const collection = []
-    let placeholder
-
-    for (const i of tracksIdList) {
-      if (trackEntity[i].artwork_url) {
-        placeholder = trackEntity[i].artwork_url
-      } else if (trackEntity[i].user === trackEntity[i].user_id) {
-        const userId = trackEntity[i].user
-        placeholder = userEntity[userId].avatar_url
-      }
-
-      collection.push(placeholder)
-    }
-
-    return collection
-  }
-
-  setBackground(url) {
-    if (url) {
-      return ({
-        background: `url(${url}) no-repeat center center`,
-        backgroundSize: 'cover'
-      })
-    }
-  }
-
-  integerToString(array) {
-    return array.map(item =>
-      item.toString(10)
-    )
-  }
-
-  renderCollection(collection) {
-    const { tracksByGenre, requested, streamIsPlaying, loadStream } = this.props
-
-    return collection.map((item, index) => {
-      const trackId = tracksByGenre[requested].ids[index]
-
-      return (
-        <Collection
-          className="gallery"
-          key={ index }
-          loadStream={ loadStream }
-          streamIsPlaying={ streamIsPlaying }
-          style={ this.setBackground(item) }
-          trackId={ trackId }
-          trackIndex={ index }
-        />
-      )
-    })
-  }
-
   render() {
     return (
       <main
@@ -75,7 +8,7 @@ export default class Main extends React.Component {
         id="main"
       >
         <div className="container">
-          { this.getCurrentCollection() }
+          { this.props.children }
         </div>
       </main>
     )
@@ -83,31 +16,5 @@ export default class Main extends React.Component {
 }
 
 Main.propTypes = {
-  loadStream: React.PropTypes.func.isRequired,
-  requested: React.PropTypes.string.isRequired,
-  streamIsPaused: React.PropTypes.bool,
-  streamIsPlaying: React.PropTypes.bool.isRequired,
-  streamTrackId: React.PropTypes.number.isRequired,
-  toggleStream: React.PropTypes.func,
-
-  trackEntity: React.PropTypes.objectOf(
-    React.PropTypes.shape({
-      artwork_url: React.PropTypes.oneOfType([
-        React.PropTypes.string.isRequired,
-        React.PropTypes.object
-      ])
-    })
-  ),
-
-  tracksByGenre: React.PropTypes.objectOf(
-    React.PropTypes.shape({
-      ids: React.PropTypes.arrayOf(React.PropTypes.number.isRequired)
-    })
-  ),
-
-  userEntity: React.PropTypes.objectOf(
-    React.PropTypes.shape({
-      avatar_url: React.PropTypes.string.isRequired
-    })
-  )
+  children: React.PropTypes.node.isRequired
 }
--- src/scripts/containers/MainContainer.jsx
+++ src/scripts/containers/MainContainer.jsx
@@ -1,26 +1,87 @@
 import React from 'react'
+import Collection from 'components/Collection'
 import Main from 'components/Main'
+import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import { loadStream, toggleStream } from 'actions/stream'
+import { requestStream } from 'actions/stream'
 
 export default class MainContainer extends React.Component {
 
+  getCollection(trackIds) {
+    const { trackEntity, userEntity } = this.props
+
+    return trackIds.map(item => {
+      if (!trackEntity[item].artwork_url) {
+        const user_id = trackEntity[item].user_id
+        return userEntity[user_id].avatar_url
+      }
+
+      return trackEntity[item].artwork_url
+    })
+  }
+
   render() {
+    const {
+      actions,
+      requested,
+      streamIsPlaying,
+      streamTrackId,
+      tracksByGenre
+    } = this.props
+
+    const collection = this.getCollection(tracksByGenre[requested].ids)
+
     return (
-      <Main
-        {...this.props}
-      />
+      <Main { ...this.props }>
+        { collection.map((item, index) => {
+          const trackId = tracksByGenre[requested].ids[index]
+          const style = {
+            background: `url(${item}) no-repeat center center`,
+            backgroundSize: 'cover'
+          }
+          return (
+            <Collection
+              actions={ actions }
+              className="gallery"
+              key={ trackId }
+              streamIsPlaying={ streamIsPlaying }
+              streamTrackId={ streamTrackId }
+              style={ style }
+              trackId={ trackId }
+            />
+          )
+        })}
+      </Main>
     )
   }
 }
 
 MainContainer.propTypes = {
-  streamTrackId: React.PropTypes.number
+  actions: React.PropTypes.shape({
+    requestStream: React.PropTypes.func.isRequired
+  }),
+  requested: React.PropTypes.string,
+  streamIsPlaying: React.PropTypes.bool,
+  streamTrackId: React.PropTypes.number,
+  trackEntity: React.PropTypes.objectOf(
+    React.PropTypes.object
+  ),
+  tracksByGenre: React.PropTypes.objectOf(
+    React.PropTypes.shape({
+      ids: React.PropTypes.arrayOf(
+        React.PropTypes.number.isRequired
+      )
+    })
+  ),
+  userEntity: React.PropTypes.objectOf(
+    React.PropTypes.object
+  )
 }
 
-const mapDispatchToProps = {
-  loadStream,
-  toggleStream
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators({ requestStream }, dispatch)
+  }
 }
 
 function mapStateToProps(state) {
@@ -32,8 +93,7 @@ function mapStateToProps(state) {
     tracksByGenre: partition.tracksByGenre,
     requested,
     streamTrackId: stream.trackId,
-    streamIsPlaying: stream.isPlaying,
-    streamIsPaused: stream.isPaused
+    streamIsPlaying: stream.isPlaying
   }
 }
 
