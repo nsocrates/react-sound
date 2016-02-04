cba970608da92241a82d4118c1ab55f6a5fe5f20
Socrates <nsocrates.dev@gmail.com>
2016-02-04 03:05:29 -0800

    Rename file to 'Gallery.jsx'

--- src/scripts/components/Gallery.jsx
+++ src/scripts/components/Gallery.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { IMG_FALLBACK } from 'constants/ItemLists'
 
-export default class Collection extends React.Component {
+export default class Gallery extends React.Component {
 
   constructor(props) {
     super(props)
     this.handleClick = this.handleClick.bind(this)
+    this.handleError = this.handleError.bind(this)
   }
 
   handleClick(e) {
@@ -19,17 +21,30 @@ export default class Collection extends React.Component {
     return actions.requestStream(trackId)
   }
 
+  handleError(e) {
+    const { currentTarget } = e
+    currentTarget.src = IMG_FALLBACK.ERROR_LARGE
+
+    return currentTarget
+  }
+
   render() {
-    const { componentClass, styles, title, user } = this.props
+    const { className, style, title, user, imgUrl } = this.props
 
     return (
-      <div className={ componentClass }>
+      <div className={ className }>
         <a
-          className="gallery__artwork"
-          href="#"
+          className="fa gallery__artwork"
+          href=""
           onClick={ this.handleClick }
-          style={ styles }
-        />
+          style={ style }
+        >
+          <img
+            className="gallery__artwork--img"
+            onError={ this.handleError }
+            src={ imgUrl }
+          />
+        </a>
         <div className="gallery__track">
           <h6 className="gallery__track--title">{ title }</h6>
           <h6 className="gallery__track--user">{ user }</h6>
@@ -39,21 +54,23 @@ export default class Collection extends React.Component {
   }
 }
 
-Collection.propTypes = {
+Gallery.propTypes = {
   actions: React.PropTypes.objectOf(
     React.PropTypes.func.isRequired
   ),
   audioIsPlaying: React.PropTypes.bool.isRequired,
-  componentClass: React.PropTypes.string,
+  className: React.PropTypes.string,
+  imgUrl: React.PropTypes.string,
   streamTrackId: React.PropTypes.number,
-  styles: React.PropTypes.objectOf(React.PropTypes.string),
+  style: React.PropTypes.objectOf(React.PropTypes.string),
   title: React.PropTypes.string,
   trackId: React.PropTypes.number,
   user: React.PropTypes.string
 }
 
-Collection.defaultProps = {
-  componentClass: null,
+Gallery.defaultProps = {
+  className: null,
+  imgUrl: null,
   style: null,
   title: 'Track Title',
   trackId: null,
